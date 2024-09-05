6e11e2c41c103544a1d986139d6b0bf43af9f2a5
Harman Singh <harman.singh@student.howest.be>
2024-09-05 18:40:12 +0200

    Show hero after Intro Animation

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
+
+
+
+const TopLogo = () => {
+    return (
+        <div className="flex items-end space-x-1">
+            <h1 className="text-3xl text-white font-semibold">Harman</h1>
+            <div className="w-1 mb-[7.5px] aspect-square bg-white"></div>
+        </div>
+    );
+}
+
+
+
+
+
+
 const Hero = () => {
 	return (
-		<div className="min-h-screen bg-black text-white">
-			<div className="flex items-end space-x-2">
-				<h1 className="text-3xl text-white font-semibold">Harman</h1>
-				<div className="w-1 mb-[18px] aspect-square bg-white"></div>
-			</div>
-		</div>
+        <div className="min-h-screen max-w-screen-xl mx-auto bg-black text-white pt-8">
+            <TopLogo />
+        </div>
 	);
 };
 
