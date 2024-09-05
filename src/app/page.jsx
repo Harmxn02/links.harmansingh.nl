e53a984420567e20d3a0f98f16103d5d5b5ac874
Harman Singh <harman.singh@student.howest.be>
2024-09-05 17:59:44 +0200

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-9xl font-semibold">Harman</h1>
+      <div className="w-2 h-2 bg-black"></div>
+    </div>
+  );
+}
