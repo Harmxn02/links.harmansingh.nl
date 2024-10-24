5f3d93db833d24039161878f01f116c8281dbc7a
Harman Singh <harmandualsingh@gmail.com>
2024-10-24 19:06:52 +0200

    UPDATE: Remove white to black transition (it hurts my eyes)

--- src/app/components/IntroAnimation.jsx
+++ src/app/components/IntroAnimation.jsx
@@ -4,15 +4,10 @@ import React, { useState, useEffect } from "react";
 import { Typewriter } from "react-simple-typewriter";
 
 const IntroAnimation = ({ onEnd }) => {
-	const [showFirstDiv, setShowFirstDiv] = useState(true);
 	const [shrinkText, setShrinkText] = useState(false);
 	const [moveOut, setMoveOut] = useState(false);
 
 	useEffect(() => {
-		const changeColour = setTimeout(() => {
-			setShowFirstDiv(false); // Switch divs
-		}, 1000);
-
 		const shrinkText = setTimeout(() => {
 			setShrinkText(true); // Shrink the text
 		}, 1500);
@@ -26,7 +21,6 @@ const IntroAnimation = ({ onEnd }) => {
 		}, 3500);
 
 		return () => {
-			clearTimeout(changeColour);
 			clearTimeout(shrinkText);
 			clearTimeout(moveOffScreen);
 			clearTimeout(removeFromDOM);
@@ -35,42 +29,13 @@ const IntroAnimation = ({ onEnd }) => {
 
 	return (
 		<div>
-			{showFirstDiv ? (
-				<div className="bg-white min-h-screen flex justify-center items-center">
-					<div className="flex items-end absolute space-x-1 md:space-x-2">
-						<h1 className="tracking-tighter md:tracking-tight text-5xl md:text-9xl text-black font-semibold">
-							<Typewriter
-								words={["Harman"]}
-								loop={false}
-								cursor={false}
-								cursorStyle="_"
-								typeSpeed={100}
-								deleteSpeed={100}
-								delaySpeed={1000}
-							/>
-							{/* Harman */}
-						</h1>
-						<div className="w-[0.4rem] mb-[7px] md:w-3 md:mb-[18px] aspect-square bg-black"></div>
-					</div>
+			<div className={`bg-black min-h-screen flex justify-center items-center transition-all duration-1000 ${moveOut ? "-translate-y-full" : "items-center"}`}>
+				<div className={`flex items-end absolute space-x-1 md:space-x-2 transition-all duration-1000 ${shrinkText ? "transform scale-50" : ""}`}>
+					<h1 className="tracking-tighter md:tracking-tight text-5xl md:text-9xl text-white font-semibold">
+						<Typewriter words={["Harman."]} loop={false} cursor={false} cursorStyle="_" typeSpeed={100} deleteSpeed={100} delaySpeed={1000} />
+					</h1>
 				</div>
-			) : (
-				<div
-					className={`bg-black min-h-screen flex justify-center items-center transition-all duration-1000 ${
-						moveOut ? "-translate-y-full" : "items-center"
-					}`}
-				>
-					<div
-						className={`flex items-end absolute space-x-1 md:space-x-2 transition-all duration-1000 ${
-							shrinkText ? "transform scale-50" : ""
-						}`}
-					>
-						<h1 className="tracking-tighter md:tracking-tight text-5xl md:text-9xl text-white font-semibold">
-							Harman
-						</h1>
-						<div className="w-[0.4rem] mb-[7px] md:w-3 md:mb-[18px] aspect-square bg-white"></div>
-					</div>
-				</div>
-			)}
+			</div>
 		</div>
 	);
 };
