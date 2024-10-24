17c8eadcbdd23a9b98d09a857128f589fcf6b818
Harman Singh <harmandualsingh@gmail.com>
2024-10-24 19:07:05 +0200

    UPDATE: Use Geist font instead of Inter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
 	};
 
 	return (
-		<div className="bg-black text-white">
+		<div className="bg-black text-white font-[family-name:var(--font-geist-sans)]">
 			{showIntro && <IntroAnimation onEnd={handleIntroEnd} />}
 			{showHero && <Hero />}
 		</div>
